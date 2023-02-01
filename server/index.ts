dccbd89ce216a50cc9af67be18390c7bf5f0821e
Aziz <aziznal.dev@gmail.com>
2023-02-01 21:06:23 +0300

    (server) setup cors for socket

--- server/index.ts
+++ server/index.ts
@@ -4,17 +4,26 @@ import { Server } from 'socket.io';
 
 import cors from 'cors';
 
-const serverPort = 3000;
-const socketPort = 3001;
+const serverPort = 4000;
+const socketPort = 4001;
 
 const app = express();
 
-const socketServer = http.createServer(app);
-const io = new Server(socketServer);
-
+app.use(cors({ origin: true }));
+app.use((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+const socketServer = http.createServer(app);
+const io = new Server(socketServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST', 'OPTIONS']
+    }
+});
 
 app.get(
     "/",
@@ -35,4 +44,14 @@ socketServer.listen(socketPort, () => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    const interval = setInterval(() => {
+        socket.emit('You are awesome!');
+        console.log("telling user they're awesome")
+    }, 1000)
+
+    socket.on("disconnect", (reason) => {
+        console.log(`Socket disconnected. Reason: ${reason}`);
+        clearInterval(interval);
+    })
   });
