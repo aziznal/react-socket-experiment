585ff68dcf220d3e2c19eb3f4dd7d9499966d203
Aziz <aziznal.dev@gmail.com>
2023-02-01 20:30:53 +0300

    (server) setup socket connection

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,14 @@
-import express, { Application, Request, Response } from "express";
+import express from "express";
+import http from 'http';
+import { Server } from 'socket.io';
 
-const app: Application = express();
-const port = 3000;
+const serverPort = 3000;
+const socketPort = 3001;
+
+const app = express();
+
+const socketServer = http.createServer(app);
+const io = new Server(socketServer);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +22,14 @@ app.get(
     }
 );
 
-app.listen(port, () => {
-    console.log(`Connected successfully on port ${port}`);
+app.listen(serverPort, () => {
+    console.log(`Connected successfully on port ${serverPort}`);
 });
+
+socketServer.listen(socketPort, () => {
+    console.log(`Connected successfully on port ${socketPort}`);
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
